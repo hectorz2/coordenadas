8813a19a39c0c292aee1cd0accd14e73c3add341
Héctor Zaragoza <hzaragoza_developer@gmail.com>
2017-09-29 16:34:41 +0200

    socket io working and minecraft fonts

--- renderer.js
+++ renderer.js
@@ -7,8 +7,19 @@ const remote = require('electron').remote;
  
 const main = remote.require('./main.js');
 
-const os = require('os');
+//const os = require('os');
+
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000', {reconnect: true});
 
 $(document).ready(function(){
-	alert(os.homedir());
+	
+	socket.on('connect', function(){
+		console.log('connected to websocket server');
+	});
+	
+      var el = $('#server-time');
+      socket.on('time', function(timeString) {
+        el.html('Server time: ' + timeString);
+      });
 });
